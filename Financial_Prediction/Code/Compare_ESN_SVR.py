117d0266106dd41edbf3da0eff52976c97ccf308
abhishekraok <abhishekrao5002@gmail.com>
2014-04-08 22:54:37 -0400

    Comparison seems to work but the value of SVR Mackey glass seems wrong

--- Financial_Prediction/Code/Compare_ESN_SVR.py
+++ Financial_Prediction/Code/Compare_ESN_SVR.py
@@ -4,7 +4,8 @@ Created on Tue Apr 08 19:38:20 2014
 
 @author: Abhishek Rao
 
-Compare Echo State Network (ESN) time series prediction with LPC and SVR.
+Compare Echo State Network (ESN) time series prediction with LPC
+Linear Ridge Regression (LRR) and Support Vector Regression (SVR).
 Data set used is sunspots and Financial data.
 Metric used is Normalized Mean Square Error (NMSE).
 """
@@ -18,7 +19,7 @@ from sklearn import linear_model
 
 svmd = SVR(C=43.6432,kernel='rbf',epsilon=0.5,gamma=1/ (2*(290.19)**2))  
 modelsets = [es.ESN(),svmd, linear_model.Ridge ()]
-modelnames = ['SVR','ESN','Linear Ridge Regression']
+modelnames = ['ESN','SVR','LRR']
 
 datasets = [sm.datasets.sunspots.load_pandas().data.ix[:,1].values,
             np.loadtxt('data/MackeyGlass_t17.txt'),
@@ -26,15 +27,16 @@ datasets = [sm.datasets.sunspots.load_pandas().data.ix[:,1].values,
                  index_col=0, parse_dates=True).ix[:,5].resample('M',
                  how='mean').values]
 datasetnames = ['Sunspots','MackayGlass','Financial']
-outputstring = ''
+ResultTable = pd.DataFrame(columns=['Model','Data','NMSE'])
 
 for X,dname in zip(*[datasets,datasetnames]):
     Xtrain,Xtest,ytrain,ytest = es.ts_train_test_split(X, lags=3)
     for ithmodel, mname in zip(*[modelsets,modelnames]):  
         model = ithmodel
-        model = ithmodel.fit(Xtrain,ytrain)
-        yp = model.predict(Xtest)
+        yp = model.fit(Xtrain,ytrain).predict(Xtest)
         NMSE = (yp-ytest).var() / ytest.var()
-        outputstring += 'The NMSE for model {0} using data {1} is {2}\n'.format(mname,dname,NMSE)
-print outputstring
-open('Model_Compare.txt','w').write(outputstring)
+        res = {'Model':mname,'Data':dname,'NMSE':NMSE}
+        ResultTable = ResultTable.append(res,ignore_index=True)               
+
+print ResultTable
+ResultTable.to_csv('ComparisonRes.csv')
